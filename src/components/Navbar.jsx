050364300bf5318fbc18ad7db8a7b02050fec7b8
Shaheer Mansoor <shaheermansoor321@gmail.com>
2023-10-22 23:37:54 +0500

    navbar setting

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,7 +26,7 @@ export default function Navbar() {
             </>
           ) : (
             <>
-              <li><Link to="/login">Login</Link></li>
+              <li style={{marginRight: "4px"}}><Link to="/login">Login</Link></li>
               <li><Link to="/signup">Signup</Link></li>
             </>
           )}
