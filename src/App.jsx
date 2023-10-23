a5901ddf559cb75440d693db9b8d8bf7ea5793b3
Shaheer Mansoor <shaheermansoor321@gmail.com>
2023-10-23 12:24:04 +0500

    minor

--- src/App.jsx
+++ src/App.jsx
@@ -14,10 +14,10 @@ function App() {
   return (
     <div className="App">
       {authIsReady && (
-        <BrowserRouter>
+        <BrowserRouter basename="/my-finance">
           <Navbar />
           <Routes>
-            <Route path="/" element={user ? <Home /> : <Navigate to="/login" />} />
+            <Route exact path="/my-finance" element={user ? <Home /> : <Navigate to="/login" />} />
             <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
             <Route path="/signup" element={user ? <Navigate to="/" /> : <Signup />} />
           </Routes>
