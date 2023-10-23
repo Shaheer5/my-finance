7fb6008fb43d43c4d3f178b1a0d19bfb8ddf2026
Shaheer Mansoor <shaheermansoor321@gmail.com>
2023-10-23 13:30:33 +0500

    minor

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         <BrowserRouter basename="/my-finance/">
           <Navbar />
           <Routes>
-            <Route path="/my-finance" element={user ? <Home /> : <Navigate to="/login" />} />
+            <Route path="/" element={user ? <Home /> : <Navigate to="/login" />} />
             <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
             <Route path="/signup" element={user ? <Navigate to="/" /> : <Signup />} />
           </Routes>
