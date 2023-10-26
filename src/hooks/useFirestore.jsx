211fac060ccbbc029cd774105da59aee81e8610f
Shaheer Mansoor <shaheermansoor321@gmail.com>
2023-10-26 20:03:41 +0500

    add: adding document functionality to firebase

--- src/hooks/useFirestore.jsx
+++ src/hooks/useFirestore.jsx
@@ -10,7 +10,12 @@ let initialState = {
 
 const firestoreReducer = (state, action) => {
   switch (action.type) {
-
+    case "IS_PENDING":
+      return {success: false, isPending: true, error: null, document: null}
+    case "ERROR":
+      return {success: false, isPending: false, error: action.payload, document: null}
+    case "ADDED_DOCUMENT":
+      return {success: true, isPending: false, error: null, document: action.payload}
     default:
       return state;
   }
@@ -23,8 +28,24 @@ export const useFirestore = (collection) => {
   // collection ref
   const ref = projectFirestore.collection(collection)
 
+  // only dispatch if not cancelled
+  const dispatchIfNotCancelled = (action) => {
+    if (!isCancelled) {
+      dispatch(action)
+    }
+  }
+
   // add a document
   const addDocument = async (doc) => {
+    dispatch({ type: "IS_PENDING" })
+
+    try {
+      const addedDocument = await ref.add({...doc})
+      dispatchIfNotCancelled({ type: "ADDED_DOCUMENT", payload: addedDocument })
+    }
+    catch (err) {
+      dispatchIfNotCancelled({ type: "ERROR", payload: err.message })
+    }
 
   }
 
